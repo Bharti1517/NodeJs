cc6580df32fbb3e3fb32f658f606ac286b102bcd
unknown <Bharti1517>
2019-05-22 18:00:35 -0700

    es6 standard

--- OS/index.js
+++ OS/index.js
@@ -6,5 +6,6 @@ console.log(os.hostname() + ' HostName ');
 console.log(os.freemem() + ' freemem ');
 
 
-console.log(' Total Memory :' + os.totalmem() + 'and freememory is : ' + os.freemem());  //ES6 Syntax 
+console.log(` Total Memory : ${os.totalmem()} and freememory is ${os.freemem()}`);  //ES6 Syntax 
 
+// console.log(' Total Memory :' + os.totalmem() + 'and freememory is : ' + os.freemem());  //ES6 Syntax
