c13e661651f12c5b34f0514c6bd4938450bb6731
unknown <Bharti1517>
2019-05-22 17:58:02 -0700

    change es6 syntax for print on console

--- OS/index.js
+++ OS/index.js
@@ -6,5 +6,5 @@ console.log(os.hostname() + ' HostName ');
 console.log(os.freemem() + ' freemem ');
 
 
-console.log(' Total Memory : ${os.totalmem()} and freememory is ${os.freemem()');  //ES6 Syntax 
+console.log(' Total Memory :' + os.totalmem() + 'and freememory is : ' + os.freemem());  //ES6 Syntax 
 
