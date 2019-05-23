c5fff2cd65117a2e4d892ac6e9bec3642cc41546
unknown <Bharti1517>
2019-05-23 14:31:02 -0700

    empty master

--- GlobalObjs/globalObj.js
+++
@@ -1,6 +0,0 @@
-//----------------Get dir path------------------
-console.log('directory Path is : ' + __dirname);
-
-//----------------Get file Path------------------
-console.log('File path is : ' + __filename);
-
--- README.md
+++
@@ -1,2 +0,0 @@
-# when u create node app steps needs to follow
-# 1.) must have package.json file Use NPM INIT 
