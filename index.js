bb892bef304729ab05586b87d728d06b96f79ecf
unknown <Bharti1517>
2019-05-23 17:30:03 -0700

    API Connect to POST man using BODY param

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ const courses = [
     {id:6 , name : 'JAVA'}
 ]
 
+
 //---------- FIRST SYNTAX FOR WRITING FUNCTION --------------------
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
+//---------- ANOTHER SYNTAX FOR WRITING FUNCTION ------------------
+//app.get('/',function(req,res){
+//})
 app.get('/api/courses',(req,res)=>{
     //res.send(['Api Courses ','Java',342,{'name':'Bharti'}]);
     res.send(courses);
@@ -29,10 +33,23 @@ app.get('/api/courses/:id',(req,res)=>{         // INPUT IN URL => http://localh
 app.get('/api/courses/:year/:month',(req,res)=>{  //INPUT IN URL => http://localhost:3000/api/courses/2019/June
     res.send(req.params); //OUTPUT is {"year":"2019","month":"June"} 
 })
+//-------------------MIDDLEWARE FOR HANDLING HTTP PORT  -------------------
+app.use(express.json(courses)); // Converting courses data to JSON  // Bind application-level middleware to an instance of the app object by using the app.use() 
+//  HANDLING HTTP PORT 
+app.post('/api/courses',(req,res)=>{
+    if(!req.body.name || req.body.name.length < 3 ){
+        // 400 Bad request 
+        res.status(400).send('Name is Required with min 3 chars ');
+        return ;
+    }
+    const course = {
+        id:courses.length+1,
+        name:req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+})
 //PORT 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log(`listening on ${port} \n http://localhost:3000  OR \n http://localhost:3000/api/courses`));
 
-//---------- ANOTHER SYNTAX FOR WRITING FUNCTION ------------------
-//app.get('/',function(req,res){
-//})
